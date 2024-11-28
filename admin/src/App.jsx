502e59133dee9ac3a49fce83b8276519aa6f615e
Karuna Bhattarai <kb.karunabhattarai110@gmail.com>
2024-11-28 16:48:35 +0545

    admin setup and initialisation

---
+++ admin/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+      <div>
+        
+      </div>
+      
+  )
+}
+
+export default App
